01264e2e20c50371b52e7c2d8afc325e9b57b58e
alexey-va <freedeeml@gmail.com>
2026-09-25 21:05:07 +0300

    build: update arc-core to 2.7.13

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,19 +24,19 @@ kotlin { jvmToolchain(25) }
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("ru.ruscrafting.arc:arc-core:2.7.11")
-    implementation("ru.ruscrafting.arc:arc-core-logging:2.7.11")
-    implementation("ru.ruscrafting.arc:arc-core-paper:2.7.11")
-    implementation("ru.ruscrafting.arc:arc-core-menu:2.7.11")
-    implementation("ru.ruscrafting.arc:arc-core-paper-menu:2.7.11")
-    compileOnly("ru.ruscrafting.arc:arc-core-paper-api:2.7.11")
+    implementation("ru.ruscrafting.arc:arc-core:2.7.13")
+    implementation("ru.ruscrafting.arc:arc-core-logging:2.7.13")
+    implementation("ru.ruscrafting.arc:arc-core-paper:2.7.13")
+    implementation("ru.ruscrafting.arc:arc-core-menu:2.7.13")
+    implementation("ru.ruscrafting.arc:arc-core-paper-menu:2.7.13")
+    compileOnly("ru.ruscrafting.arc:arc-core-paper-api:2.7.13")
     compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
     compileOnly("com.github.angeschossen:LandsAPI:7.25.4")
     compileOnly("ru.ruscrafting.thirdparty:elitemobs-api:10.1.1")
 
     testImplementation(kotlin("test"))
-    testImplementation("ru.ruscrafting.arc:arc-core-paper-testing:2.7.11")
-    testImplementation("ru.ruscrafting.arc:arc-core-paper-api:2.7.11")
+    testImplementation("ru.ruscrafting.arc:arc-core-paper-testing:2.7.13")
+    testImplementation("ru.ruscrafting.arc:arc-core-paper-api:2.7.13")
     testImplementation("io.mockk:mockk:1.14.7")
 }
 
